9061e1551d485e14492130c378df3d25b6ea37da
mariam <mariamelmsery7@gmail.com>
2024-06-01 21:44:58 +0300

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -20,22 +20,23 @@ function App() {
   }, [darkMode]);
 
   return (
-    // <div
-    //   className={`bg-sidebar text-textcolor1 dark:bg-gray-900 dark:text-gray-100 min-h-screen`}
-    // >
-    //   <header className="p-4 bg-landing dark:bg-gray-800">
-    //     <h1 className="text-xl font-bold">App</h1>
-    //     <button
-    //       className="ml-auto bg-button text-white dark:bg-gray1 px-4 py-2 rounded"
-    //       onClick={() => setDarkMode(!darkMode)}
-    //     >
-    //       Toggle Dark Mode
-    //     </button>
-    //   </header>
-    // </div>
-
+    
     <>
     <ToastContainer />
+    <div
+      className={`bg-sidebar text-textcolor1 dark:bg-gray-900 dark:text-gray-100 min-h-screen`}
+    >
+      <header className="p-4 bg-landing dark:bg-gray-800">
+        <h1 className="text-xl font-bold">App</h1>
+        <button
+          className="ml-auto bg-button text-white dark:bg-gray1 px-4 py-2 rounded"
+          onClick={() => setDarkMode(!darkMode)}
+        >
+          Toggle Dark Mode
+        </button>
+      </header>
+    </div> 
+
     
     </>
   );
